dfa034ab41fc4092d6df2a5d6501f2f98cdda5c8
russo <pasquale.russo@eng.it>
2014-05-14 15:26:07 +0200

    init

---
+++ pkgdiff-ui/src/main/java/it/eng/pkgdiff/MainBean.java
@@ -0,0 +1,17 @@
+package it.eng.pkgdiff;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class MainBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public String getVersion() {
+		return "1.0";
+	}
+}
