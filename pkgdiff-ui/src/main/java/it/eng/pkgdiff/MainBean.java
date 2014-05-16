de7f3fe63566eddae5214341c684aae5b10fe4aa
s227902 <s227902@FPASQUALW.italy.itroot.adnet>
2014-05-16 17:41:50 +0200

    Aggiunta la gestione dei tasti per l'upload e confronto

--- pkgdiff-ui/src/main/java/it/eng/pkgdiff/MainBean.java
+++ pkgdiff-ui/src/main/java/it/eng/pkgdiff/MainBean.java
@@ -1,9 +1,18 @@
 package it.eng.pkgdiff;
 
+import java.io.File;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.icefaces.ace.component.fileentry.FileEntry;
+import org.icefaces.ace.component.fileentry.FileEntryEvent;
+import org.icefaces.ace.component.fileentry.FileEntryResults;
+import it.eng.pkgdiff.batch.*;
 
 /**
  * MasterBean del progetto
@@ -11,13 +20,113 @@ import javax.faces.bean.ViewScoped;
  *
  */
 
-@ManagedBean
+@ManagedBean(name= MainBean.BEAN_NAME)
 @ViewScoped
 public class MainBean implements Serializable {
-
 	private static final long serialVersionUID = 1L;
+	public static final String BEAN_NAME = "mainBean";
+	
+	private List<String> fileData;
+	
+	private String groupId;
+    private String artifactId;
+    private String versionId;
+    
+
+	
 
 	public String getVersion() {
 		return "1.0";
 	}
+	
+	
+	public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String aGroupId) {
+        this.groupId = aGroupId;
+    }
+
+	public String getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(String anArtifactId) {
+        this.artifactId = anArtifactId;
+    }
+
+    public String getVersionId() {
+        return versionId;
+    }
+
+    public void setVersionId(String aVersionId) {
+        this.versionId = aVersionId;
+    }
+    
+	// utilizzata dal fileEntry
+	public void sampleListener(FileEntryEvent e)
+    {
+		System.out.println("sampleListener");
+        FileEntry fe = (FileEntry)e.getComponent();
+        FileEntryResults results = fe.getResults();
+        File parent = null;
+
+        fileData = new ArrayList<String>();
+
+    //get data About File
+    
+        for (FileEntryResults.FileInfo i : results.getFiles()) 
+        {
+            fileData.add("File Name: " + i.getFileName());
+
+            if (i.isSaved()) {
+                fileData.add("File Size: " + i.getSize() + " bytes");
+
+                File file = i.getFile();
+                if (file != null) {
+                    parent = file.getParentFile();
+                }
+            } else {
+                fileData.add("File was not saved because: " +
+                    i.getStatus().getFacesMessage(
+                        FacesContext.getCurrentInstance(),
+                        fe, i).getSummary());
+            }
+        }
+
+        if (parent != null) {
+            long dirSize = 0;
+            int fileCount = 0;
+            for (File file : parent.listFiles()) {
+                fileCount++;
+                dirSize += file.length();
+            }
+            fileData.add("Total Files In Upload Directory: " + fileCount);
+            fileData.add("Total Size of Files In Directory: " + dirSize + " bytes");
+        }
+    }
+
+	// utilizzata dal fileEntry
+    public List getFileData() {
+        return fileData;
+    }
+    
+	// utilizzata dal Bottone di Confronta Archive
+	public String executeButtonConfronta() {        	
+		System.out.println("Effettuo Download per groupId:"+groupId);
+		System.out.println("artifactId:"+artifactId);
+		System.out.println("versionId:"+versionId);
+		try {
+			// Sostituire la url corretta letta del properties
+			DownloadMavenDependency.download(groupId, artifactId,versionId,"http://161.27.213.71:8081/nexus/content/groups/public");
+		} catch (Exception e){}
+		
+		// Sostituire i pkg Corretti e i path Corretti
+		System.out.println("Effettuo ExecPkgDiff");
+		ExecPkgDiff.execute("PrestazioniEJB.jar",
+				"PrestazioniEJB.jar", "target",
+				"changes_report.html",System.out);		
+        return null;
+    }
 }
